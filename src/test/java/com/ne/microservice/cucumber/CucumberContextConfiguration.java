3c5855fb2ed314632d41fdf5775745d1c68bf42c
rternovyi <roman.ternovyy@gmail.com>
2020-04-29 12:47:11 +0300

    Initial version of nemicroservice generated by JHipster-6.8.0

---
+++ src/test/java/com/ne/microservice/cucumber/CucumberContextConfiguration.java
@@ -0,0 +1,20 @@
+package com.ne.microservice.cucumber;
+
+import com.ne.microservice.NemicroserviceApp;
+import io.cucumber.java.Before;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@SpringBootTest
+@WebAppConfiguration
+@ContextConfiguration(classes = NemicroserviceApp.class)
+public class CucumberContextConfiguration {
+
+    @Before
+    public void setup_cucumber_spring_context() {
+        // Dummy method so cucumber will recognize this class as glue
+        // and use its context configuration.
+    }
+
+}
